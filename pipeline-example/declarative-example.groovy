b33b9d2bd510dc8cd60577d4f2015eb334d9be86
kjoh94 <kjoh94@gmail.com>
2017-09-23 14:25:52 +0900

    update

--- pipeline-example/declarative-example.groovy
+++ pipeline-example/declarative-example.groovy
@@ -2,6 +2,8 @@ pipeline {
     agent any
     parameters {
         string(name: 'PERSON', defaultValue: 'Mr Jenkins', description: 'Who should I say hello to?')
+        string(name: 'ID', defaultValue: 'ID', description: 'my id')
+        booleanParam(defaultValue: true, description: '', name: 'flag')
     }
     stages {
         stage('Example') {
