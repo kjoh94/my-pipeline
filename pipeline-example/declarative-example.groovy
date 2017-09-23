898a17e782fbd63d293beca01adca1c6c0555b26
kjoh94 <kjoh94@gmail.com>
2017-09-23 14:42:15 +0900

    .

--- pipeline-example/declarative-example.groovy
+++ pipeline-example/declarative-example.groovy
@@ -4,6 +4,7 @@ pipeline {
         string(name: 'PERSON', defaultValue: 'Mr Jenkins', description: 'Who should I say hello to?')
         string(name: 'ID', defaultValue: 'ID', description: 'my id')
         booleanParam(defaultValue: true, description: '', name: 'flag')
+        choice(choices: 'US-EAST-1\nUS-WEST-2', description: 'What AWS region?', name: 'region')
     }
     stages {
         stage('Example') {
