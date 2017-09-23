1f653d279e049e253be144b0e8242ab4dc8fb85d
kjoh94 <kjoh94@gmail.com>
2017-09-23 11:59:54 +0900

    .

--- pipeline-example/declarative-example.groovy
+++ pipeline-example/declarative-example.groovy
@@ -3,6 +3,7 @@ pipeline {
     parameters {
         string(name: 'PERSON', defaultValue: 'Mr Jenkins', description: 'Who should I say hello to?'),
         string(name: 'MYID', defaultValue: '1234', description: 'my id'),
+        string(name: 'MYAGE', defaultValue: '12', description: 'my age')
     }
     stages {
         stage('Example') {
