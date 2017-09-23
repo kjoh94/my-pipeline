ef8719bfb5c53eef983805ff4a608e08f323e445
kjoh94 <kjoh94@gmail.com>
2017-09-23 15:03:00 +0900

    .

--- pipeline-example/test1.groovy
+++ pipeline-example/test1.groovy
@@ -9,10 +9,13 @@ node('slave1') {
     echo 'Hello World'
     echo "workspace=${env.WORKSPACE}"
 
+    env.WORKBASE = env.WORKSPACE.replace(env.JOB_NAME, '')
+    echo "workbase=${env.WORKBASE}"
+
     dir (env.WORKSPACE)
     {
         fileOperations([fileCreateOperation(fileContent: '1234', fileName: 'a.txt')])
-        //sleep time: 10, unit: 'MINUTES'
+        sleep time: 10, unit: 'MINUTES'
         echo "I'm sleeping..."
     }
 }
