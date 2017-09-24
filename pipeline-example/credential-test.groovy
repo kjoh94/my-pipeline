6e57215e54eaa73eb6696280c8390754ada69132
kjoh94 <kjoh94@gmail.com>
2017-09-24 15:38:12 +0900

    .

--- pipeline-example/credential-test.groovy
+++ pipeline-example/credential-test.groovy
@@ -27,7 +27,7 @@ node('slave1') {
         }
         echo "USERNAME=${env.USERID}"
 
-        withCredentials([perforcePasswordCredential(credentialsId: '97dd51f7-a592-4145-90fd-efff461f8c05', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+        withCredentials([usernamePassword(credentialsId: '97dd51f7-a592-4145-90fd-efff461f8c05', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
             env.USERID = USERNAME
         }
         echo "USERNAME=${env.USERID}"
